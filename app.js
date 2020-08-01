13263741c7a8511c085b0505d3452ce119ab3a35
Alexis Mamani <alexis.mamani@oktana.io>
2020-08-01 15:31:24 -0300

    primer commit

---
+++ app.js
@@ -0,0 +1,16 @@
+
+const http = require('http');
+
+http.createServer((req, res) => {
+  res.writeHead(200, { 'Content-Type': 'application/json'});
+  let output = {
+    nombre: 'alexis',
+    edad: 25,
+    url: req.url
+  }
+  res.write(JSON.stringify(output));
+  res.end();
+})
+.listen(8080);
+
+console.log('listening port 8080');
